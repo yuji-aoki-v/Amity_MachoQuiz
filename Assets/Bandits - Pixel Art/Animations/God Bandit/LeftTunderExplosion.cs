1d9a233bca86a21620505099384083ff051c2944
ksugi <k.sugino0203@gmail.com>
2025-02-18 20:21:50 +0900

    leftThunder実装

--- Assets/Bandits - Pixel Art/Animations/God Bandit/LeftTunderExplosion.cs
+++ Assets/Bandits - Pixel Art/Animations/God Bandit/LeftTunderExplosion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AquareExplosion : MonoBehaviour
+public class LeftTunderExplosion : MonoBehaviour
 {
     [SerializeField] GameObject explosionPrefab;
 
